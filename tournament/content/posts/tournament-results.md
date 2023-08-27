b8d076d94557aba7f10a742d665d303c04962e58
Toothyca <Toothyca@gmail.com>
2023-08-27 00:29:11 -0700

    Revert "initial commit"
    This reverts commit 5e8d23ac73f3451d35793602c78b2ed12b7209b1.

--- tournament/content/posts/tournament-results.md
+++
@@ -1,9 +0,0 @@
----
-title: "Tournament Results"
-date: 2023-08-27T00:15:58-07:00
-draft: true
----
-
-## Introduction
-
-Hey **guys**, this is the tournament *results*.
--- tournaments/convert-php.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-php_files = ["gif.php"]#, "index.php", "match.php", "matcharchive.php", "matchstats.php", "output.php", "relay.php", "tournament.php"]
-
-for php_file in php_files:
-    output = subprocess.check_output(["php", php_file], universal_newlines=True)
-    with open(f"{php_file.replace('.php', '.html')}", "w") as f:
-        f.write(output)
